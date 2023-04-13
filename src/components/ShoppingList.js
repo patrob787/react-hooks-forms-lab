d781a0f7f3865534d2026ce2abed0938d5aa165a
Patrick Roberts <patrob22@gmail.com>
2023-04-13 00:41:11 -0400

    first deliverable

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,12 +5,29 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchField, setSearchField] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function onSearchChange(e) {
+    setSearchField(e.target.value)
+  }
+
+  function renderItems(itemArray) {
+    return itemArray.map((item) => (
+      <Item key={item.id} name={item.name} category={item.category} />
+    ))
+  }
+
+  const searchItems = items.filter((item) => {
+    if (item.name.toLowerCase().includes(searchField.toLowerCase())) {
+      return item
+    }
+  })
+  
+  const itemsToDisplay = items.filter((item) => { 
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
@@ -19,11 +36,11 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter  
+        onSearchChange={onSearchChange}
+        onCategoryChange={handleCategoryChange} />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {searchField ? renderItems(searchItems) : renderItems(itemsToDisplay)}
       </ul>
     </div>
   );
